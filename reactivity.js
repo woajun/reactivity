9e1dc73b27bb543f9871dbd648c2c1dc107580af
juni <woajun@naver.com>
2022-07-11 11:17:53 +0900

    refForA0

--- reactivity.js
+++ reactivity.js
@@ -1,18 +1,19 @@
-const A0 = ref(1);
-const A1 = ref(2);
-let A2 = A0.value + A1.value;
+const A0 = refForA0(1);
+const A1 = 2;
+let A2 = A0.value + A1;
 
 console.log(A2); // 3
 
 A0.value = 2;
 console.log(A2); // still 3, but i want 4
 
-function ref(value) {
+function refForA0(value) {
   const obj = {
     get value() {
       return value;
     },
     set value(newValue) {
+      A2 = newValue + A1;
       value = newValue;
     },
   };
