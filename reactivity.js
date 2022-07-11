b347a5c1f6908fefe195b2e1b7c8b6bfd4ca038b
juni <woajun@naver.com>
2022-07-11 11:12:20 +0900

    add ref

--- reactivity.js
+++ reactivity.js
@@ -1,8 +1,16 @@
-let A0 = 1;
-let A1 = 2;
-let A2 = A0 + A1;
+const A0 = ref(1);
+const A1 = ref(2);
+let A2 = A0.value + A1.value;
 
 console.log(A2); // 3
 
-A0 = 2;
+A0.value = 2;
 console.log(A2); // still 3
+
+function ref(value) {
+  const obj = {
+    value: value,
+  };
+
+  return obj;
+}
